8d6938cf422f5aee717b086c9d1a1f89567c1439
agostinhobritojr <agostinhobritojr@gmail.com>
2014-11-13 08:55:23 -0300

    comentando mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -44,86 +44,100 @@ class MainWindow;
  * fft, audio probing, widget properties.
  */
 class MainWindow : public QMainWindow{
-    Q_OBJECT
-    
+  Q_OBJECT
+
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+  /**
+   * @brief MainWindow is the class constructor. It prepares the
+   * whole player for full operation
+   * @param parent
+   */
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
 
 public slots:
   // what to do when user select a new song to play
+  /**
+   * @brief goToItem tells the player to play the selected song on the playlist
+   * @param index stores the index of the song
+   */
+  void goToItem(const QModelIndex &index);
 
-    void goToItem(const QModelIndex &index);
-    void loadMedia();
-    void loadPlaylist();
-    void onAddFolderToLibrary();
-    void onAddMediaToPlayList(QString media);
-    void mediaStatusChanged(QMediaPlayer::MediaStatus status);
-    void metaDataChanged();
-    void next();
-    void playPause();
-    void slotPositionChanged(qint64 e);
-    void prev();
-    void processBuffer(QAudioBuffer buffer);
-    void setMediaAt(qint32 percent);
-    void setVolume(int volume);
-    void spectrumAvailable(QVector<double> spectrum);
-    void metaDataAvailableChanged(bool);
+  /**
+   * @brief loadMedia allows the user to oad a new media selected by the user
+
+   */
+  void loadMedia();
+  void loadPlaylist();
+  void mediaStatusChanged(QMediaPlayer::MediaStatus status);
+  void metaDataAvailableChanged(bool);
+  void metaDataChanged();
+  void next();
+  void onAddFolderToLibrary();
+  void onAddMediaToPlayList(QString media);
+  void playPause();
+  void prev();
+  void processBuffer(QAudioBuffer buffer);
+  void setMediaAt(qint32 percent);
+  void setVolume(int volume);
+  void slotPositionChanged(qint64 e);
+  void spectrumAvailable(QVector<double> spectrum);
+
 private:
-    // User interface widget
-    Ui::MainWindow *ui;
+  // User interface widget
+  Ui::MainWindow *ui;
 
-    // pointer to the media player object
-    QMediaPlayer *player;
+  // pointer to the media player object
+  QMediaPlayer *player;
 
-    // stores the playlist
-    QMediaPlaylist *playlist;
+  // stores the playlist
+  QMediaPlaylist *playlist;
 
-    // audio info... we do not use it
-    QAudioDeviceInfo audioInfo;
+  // audio info... we do not use it
+  QAudioDeviceInfo audioInfo;
 
-    // a buffer to copy audio into it
-    QByteArray buffer;
+  // a buffer to copy audio into it
+  QByteArray buffer;
 
-    // the audio prober
-    QAudioProbe *probe;
+  // the audio prober
+  QAudioProbe *probe;
 
-    // a fft calculator object
-    FFTCalc *calculator;
+  // a fft calculator object
+  FFTCalc *calculator;
 
-    // item model to design the playlist into mainwindow
-    QStandardItemModel *model;
+  // item model to design the playlist into mainwindow
+  QStandardItemModel *model;
 
-    // each item to be displayed in playlist
-    QStandardItem *item;
+  // each item to be displayed in playlist
+  QStandardItem *item;
 
-    // input sample to fft calc
-    QVector<double> sample;
+  // input sample to fft calc
+  QVector<double> sample;
 
-    // output vector with spectrum
-    QVector<double> spectrum;
+  // output vector with spectrum
+  QVector<double> spectrum;
 
-    PlaylistModel *playlistModel;
+  PlaylistModel *playlistModel;
 
-    // left and right mean levels
-    double levelLeft, levelRight;
+  // left and right mean levels
+  double levelLeft, levelRight;
 signals:
-    // telle a new buffer from audio prober
-    int spectrumChanged(QVector<double> &sample);
+  // telle a new buffer from audio prober
+  int spectrumChanged(QVector<double> &sample);
 
-    // music position changed by user. Tell
-    // new position to the player
-    int positionChanged(qint64 position);
+  // music position changed by user. Tell
+  // new position to the player
+  int positionChanged(qint64 position);
 
-    // tells the new left and right mean audio levels
-    int levels(double left, double right);
+  // tells the new left and right mean audio levels
+  int levels(double left, double right);
 
-    // tells the duration of media
-    // when a new media is played
-    int elapsedTimeChanged(qint64 elapsed);
+  // tells the duration of media
+  // when a new media is played
+  int elapsedTimeChanged(qint64 elapsed);
 
-    // tells there are new directories to be added to the music library
-    int addFolderToLibrary(QString folder);
+  // tells there are new directories to be added to the music library
+  int addFolderToLibrary(QString folder);
 
 protected slots:
 };
